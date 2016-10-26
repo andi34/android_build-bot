5490cb10acbfb7c225daca9d9cdc00418dc1d265
Andreas Blaesius <skate4life@gmx.de>
2016-10-26 14:17:33 +0200

    Add SlimKat config

---
+++ kk4.4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BRANCH=kk4.4
+STORAGE=~/android/roms/slimroms
+VER=4.4.4
+ROM=Slim
+TAB2CHANGES=n
+STABLEKERNEL=y
+LUNCHROM=slim
+JAVAVERTARGET=7
+HOWCLEAN=clobber
+BUILD_TARGETS="bacon"
+UPLOADCROWDIN=n
+
+. `dirname $0`/bot.sh
