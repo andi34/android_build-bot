ab97aead4deaacb09d1043ecc3ef9b06fbd59ac7
Andreas Blaesius <skate4life@gmx.de>
2017-01-11 10:15:13 +0100

    Update TWRP script
    - add some colors
    - always move files, always create a flashable zip

--- twrp.sh
+++ twrp.sh
@@ -7,6 +7,30 @@
 # Thanks to ketut.kumajaya @ XDA
 #
 
+readonly red=$(tput setaf 1) #  red
+readonly grn=$(tput setaf 2) #  green
+readonly ylw=$(tput setaf 3) #  yellow
+readonly blu=$(tput setaf 4) #  blue
+readonly cya=$(tput setaf 6) #  cyan
+readonly txtbld=$(tput bold) # Bold
+readonly bldred=$txtbld$red  #  red
+readonly bldgrn=$txtbld$grn  #  green
+readonly bldylw=$txtbld$ylw  #  yellow
+readonly bldblu=$txtbld$blu  #  blue
+readonly bldcya=$txtbld$cya  #  cyan
+readonly txtrst=$(tput sgr0) # Reset
+
+err() {
+	echo "$txtrst${red}$*$txtrst" >&2
+}
+
+warn() {
+	echo "$txtrst${ylw}$*$txtrst" >&2
+}
+info() {
+	echo "$txtrst${grn}$*$txtrst"
+}
+
 #---------------------Device Settings------------------#
 
 # device name in your "out" folder
@@ -40,9 +64,6 @@ RECVER="3.0.3-0"
 #(*.img and *.zip will also get moved if MOVE=y)
 STORAGE=~/android/roms/recovery
 
-# Create a flashable zip?
-FLASHZIP=y
-
 # Place your pre-flashable-zip in this path. Nameing of the zip: DEVICENAME1-RECNAME1.zip
 ZIPBASE=~/android/roms/recovery_base
 
@@ -66,35 +87,31 @@ breakfast ${DEVICENAME1[$VAL]}
 
 make -j8 recoveryimage
 
-echo "Moving to out directory ( $OUT ) ..."
+info "Moving to out directory ( $OUT ) ..."
 cd $OUT
 
-echo "Converting recovery.img to a odinflashable *.tar.md5 ..."
+info "Converting recovery.img to a odinflashable *.tar.md5 ..."
 tar -H ustar -c recovery.img > ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar"
 md5sum -t ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar" >> ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar"
 mv ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar" ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar.md5"
 
-if [ $MOVE = "y" ]; then
-echo "Moveing odin flashable *.tar.md5...."
+info "Moveing odin flashable *.tar.md5...."
 mkdir -p $STORAGE/${DEVICENAME1[$VAL]}
 mv *".tar.md5" $STORAGE/${DEVICENAME1[$VAL]}/
 cp "recovery.img" $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".img"
-fi
 
-if [ $FLASHZIP = "y" ]; then
-echo "Makeing flashable zip..."
+info "Makeing flashable zip..."
 mkdir -p $STORAGE/${DEVICENAME1[$VAL]}
 cp $ZIPBASE/${DEVICENAME1[$VAL]}"-"$RECNAME1".zip" $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".zip"
 zip -g $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".zip" "recovery.img"
-fi
 
-echo "Moving back to source directory..."
+info "Moving back to source directory..."
 cd $SAUCE
 
-echo "Make clobber"
+warn "Make clobber"
 make clobber
 
 done
 
-echo "Done!"
+info "Done!"
 
