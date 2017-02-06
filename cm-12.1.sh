e95a7be623af80394a58cece087dd970c9b35ec7
Andreas Blaesius <skate4life@gmx.de>
2017-02-06 19:47:39 +0100

    Update cm-12.1

--- cm-12.1.sh
+++ cm-12.1.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+BRANCH=cm-12.1
 STORAGE=~/android/roms/cm
 VER=5.1.1
-ROM=cm-12
-SAUCE=~/android/cm-12.1
+ROM=cm-12.1
 TAB2CHANGES=y
 STABLEKERNEL=y
 LUNCHROM=cm
-SECONDOUTPATH=/ssd2/out/cm-12.1
 JAVAVERTARGET=7
 CLEAN_TARGETS=clobber
 BUILD_TARGETS="bacon"
