1841e77e7f1dc0d86662f71678dbaa66262fdfd9
Andreas Blaesius <skate4life@gmx.de>
2017-04-15 15:26:12 +0200

    aosp: avoid using espressowifi and fallback to espresso

--- aosp-7.1.sh
+++ aosp-7.1.sh
@@ -13,12 +13,17 @@ CLEAN_TARGETS=clobber
 BUILD_TARGETS="otapackage"
 
 if [[ ! -z $1 ]]; then
-	ONEDEVICEONLY=y
-	PRODUCT[0]=$1
-	echo "Compiling for ${PRODUCT[$VAL]} only"
+    ONEDEVICEONLY=y
+    if [ "$1" = "espressowifi" ]; then
+        echo "assuming espresso device, espressowifi does not exist on UA"
+        PRODUCT[0]=espresso
+    else
+        PRODUCT[0]=$1
+    fi
+    echo "Compiling for ${PRODUCT[$VAL]} only"
 else
-	echo "Compiling all supported devices"
-	ONEDEVICEONLY=n
+    echo "Compiling all supported devices"
+    ONEDEVICEONLY=n
 fi
 
 . `dirname $0`/bot.sh
