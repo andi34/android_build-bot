e6f49555908fa30efd65bd43ec6cdafcbc00ec92
Andreas Blaesius <skate4life@gmx.de>
2017-09-22 19:42:44 +0200

    Add support for single device & golden

--- twrp.sh
+++ twrp.sh
@@ -33,22 +33,40 @@ info() {
 
 #---------------------Device Settings------------------#
 
-# device name in your "out" folder
-DEVICENAME1[0]="espressocommon"
-DEVICENAME1[1]="maguro"
-DEVICENAME1[2]="toro"
-DEVICENAME1[3]="toroplus"
-#DEVICENAME1[4]="p5110"
-#DEVICENAME1[5]="golden"
 
-# 2nd device name to rename the *.tar.md5 and *.zip the right way
-# the odin-flashable tar.md5 will be name like this: DEVICENAME2_RECNAME1_RECVER.tar
-DEVICENAME2[0]="espresso-common"
-DEVICENAME2[1]="maguro"
-DEVICENAME2[2]="toro"
-DEVICENAME2[3]="toroplus"
-#DEVICENAME2[3]="GT-P5110"
-#DEVICENAME2[4]="GT-I8190"
+
+if [[ ! -z $1 ]]; then
+    if [ "$1" = "golden" ]; then
+        DEVICENAME1[0]=golden
+        DEVICENAME2[0]="GT-I8190"
+        echo "Compiling for ${DEVICENAME1[$VAL]} only"
+        startcompile
+    elif [[ ! -z $2 ]]; then
+        DEVICENAME1[0]=$1
+        DEVICENAME2[0]=$2
+        echo "Compiling for ${DEVICENAME1[$VAL]} only"
+        startcompile
+    else
+        echo "${DEVICENAME2[$VAL]} not set!"
+        abortcompile
+    fi
+else
+    # device name in your "out" folder
+    DEVICENAME1[0]="espressocommon"
+    DEVICENAME1[1]="maguro"
+    DEVICENAME1[2]="toro"
+    DEVICENAME1[3]="toroplus"
+
+    # 2nd device name to rename the *.tar.md5 and *.zip the right way
+    # the odin-flashable tar.md5 will be name like this: DEVICENAME2_RECNAME1_RECVER.tar
+    DEVICENAME2[0]="espresso-common"
+    DEVICENAME2[1]="maguro"
+    DEVICENAME2[2]="toro"
+    DEVICENAME2[3]="toroplus"
+
+    startcompile
+fi
+
 
 #---------------------General Settings------------------#
 
@@ -58,7 +76,7 @@ DEVICENAME2[3]="toroplus"
 RECNAME1="TWRP"
 
 # recovery version number
-export TW_DEVICE_VERSION=0
+export TW_DEVICE_VERSION=1
 RECVER="3.1.1-$TW_DEVICE_VERSION"
 
 # path to move the *.tar.md5 
@@ -77,45 +95,53 @@ SAUCE=~/android/android-7.1
 #---------------------Convert & Move Code----------------#
 # Very much not a good idea to change this unless you know what you are doing....
 
-cd $SAUCE
-. build/envsetup.sh
-make clobber
-for VAL in "${!DEVICENAME1[@]}"
-do
-cd $SAUCE
+startcompile() {
+	cd $SAUCE
+	. build/envsetup.sh
+	make clobber
+	for VAL in "${!DEVICENAME1[@]}"
+	do
+	cd $SAUCE
 
-breakfast ${DEVICENAME1[$VAL]}
+	breakfast ${DEVICENAME1[$VAL]}
 
-make -j8 recoveryimage
+	make -j8 recoveryimage
 
-info "Moving to out directory ( $OUT ) ..."
-cd $OUT
+	info "Moving to out directory ( $OUT ) ..."
+	cd $OUT
 
-mkdir -p $STORAGE/${DEVICENAME1[$VAL]}
+	mkdir -p $STORAGE/${DEVICENAME1[$VAL]}
 
-if [ "$DEVICENAME1" = "espressocommon" ]; then
-	info "Converting recovery.img to a odinflashable *.tar.md5 ..."
-	tar -H ustar -c recovery.img > ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar"
-	md5sum -t ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar" >> ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar"
-	mv ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar" ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar.md5"
+	if [ "${DEVICENAME1[$VAL]}" = "espressocommon" ]; then
+		info "Converting recovery.img to a odinflashable *.tar.md5 ..."
+		tar -H ustar -c recovery.img > ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar"
+		md5sum -t ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar" >> ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar"
+		mv ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar" ${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".tar.md5"
 
-	info "Moveing odin flashable *.tar.md5..."
-	mv *".tar.md5" $STORAGE/${DEVICENAME1[$VAL]}/
+		info "Moveing odin flashable *.tar.md5..."
+		mv *".tar.md5" $STORAGE/${DEVICENAME1[$VAL]}/
 
-	info "Makeing flashable zip..."
-	cp $ZIPBASE/${DEVICENAME1[$VAL]}"-"$RECNAME1".zip" $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".zip"
-	zip -g $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".zip" "recovery.img"
-fi
+		info "Makeing flashable zip..."
+		cp $ZIPBASE/${DEVICENAME1[$VAL]}"-"$RECNAME1".zip" $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".zip"
+		zip -g $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".zip" "recovery.img"
+	fi
 
-info "Moveing recovery.img..."
-cp "recovery.img" $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".img"
+	info "Moveing recovery.img..."
+	cp "recovery.img" $STORAGE/${DEVICENAME1[$VAL]}/${DEVICENAME2[$VAL]}"_"$RECNAME1"_"$RECVER".img"
 
-info "Moving back to source directory..."
-cd $SAUCE
+	info "Moving back to source directory..."
+	cd $SAUCE
 
-warn "Make clobber"
-make clobber
+	warn "Make clobber"
+	make clobber
 
-done
+	done
 
-info "Done!"
+	info "Done!"
+}
+
+abortcompile() {
+	warn "####################"
+	warn "#      FAILED!     #"
+	warn "####################"
+}
