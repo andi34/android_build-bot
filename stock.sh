3a3b45019a10fec9f6b8abbf99df575a0edc9a17
Andreas Blaesius <andi@unlegacy-android.org>
2020-05-01 09:27:40 +0200

    Delete unused config for stock kernel compilation
    Change-Id: I7ab8fea7effbbec9d47d069274d21e084bbd9fa3

--- stock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SAUCE=~/android/kernelcompile
-PVRSAUCE=~/android/aosp-6.0/hardware/ti/omap4/pvr-source/eurasiacon
-KERNELSOURCE=~/android/kernelcompile/samsung_espresso_kernel
-DEFCONFIGNAME=android_espresso10_omap4430_r02_user_defconfig
-WORKINGDIR=$SAUCE/out/$DEFCONFIGNAME
-WORKINGOUTDIR=$SAUCE/$DEFCONFIGNAME-bin
-. `dirname $0`/compile-espresso.sh
